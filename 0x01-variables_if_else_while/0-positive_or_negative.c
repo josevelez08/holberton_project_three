ec1e2e876efef09ffcf3ba0ec7c1c019f76507eb
Jose Velez <jose-0919@hotmail.com>
2020-06-11 16:52:59 -0500

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,7 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 
 /**
  * main - positive and negative
@@ -22,7 +24,8 @@ int main(void)
 		if (n == 0)
 		{
 			printf(" is zero");
-		} else {
+		} else
+		{
 			printf(" is negative");
 		}
 	}
