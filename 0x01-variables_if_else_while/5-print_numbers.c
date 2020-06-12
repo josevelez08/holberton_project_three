dae940efe456fc9d5f02dfd2e066179ff7105c44
Jose Velez <jose-0919@hotmail.com>
2020-06-12 15:36:29 -0500

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
+
 /**
  * main - digits
  *
  * Return: 0 always runs
  */
 int main(void)
-{
-	int n = 0;
-
-	for (n = 0; n < 10; n++)
 	{
-		printf("%d", n);
-	}
-	printf("\n")
-	return (0);
-}
+		int n = 0;
+
+		for (n = 0; n < 10; n++)
+		{
+			printf("%d", n);
+		}
+		printf("\n");
+		return (0);
+}	
