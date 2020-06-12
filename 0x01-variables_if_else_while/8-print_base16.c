0afa3c966a316edfd02ac243060c55a02bd29b4e
Jose Velez <jose-0919@hotmail.com>
2020-06-12 16:25:00 -0500

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,15 +7,15 @@
 	int main(void)
 	{
 		int i = 0;
-		
-				for (i = 48; i < 58; i++)
-			{
-			putchar (i);
-			}
-		for (i = 97; i < 103; i++)
-			{
+			
+			for (i = 48; i < 58; i++)
+				{
 				putchar (i);
-			}
-	 putchar (10) 
-		return (0);
-	}
+				}
+			for (i = 97; i < 103; i++)
+				{
+					putchar (i);
+				}
+			putchar (10);
+			return (0);
+		}
