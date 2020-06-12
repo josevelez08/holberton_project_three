38997c2aece360310a77cf2f97eb43ccc650389c
Jose Velez <jose-0919@hotmail.com>
2020-06-12 13:36:25 -0500

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,23 +1,22 @@
-
 #include <stdio.h>
 /**
- * main - alfabeto mayusculas y minisculas
- *
- * Return: 0 always runs
- */
-int main(void)
-{
+	 * main - alfabeto mayusculas y minisculas
+	 *
+	 * Return: 0 always runs
+	 */
+	int main(void)
+	{
 		int i;
+		
 
-		for (i = 97; i < 123; i++)
-		{
+		for (i = 97; i < 123; ++i)
+			{
+			putchar (i);
+			}	
+		for (i = 65; i < 91; ++i)
+			{
 				putchar (i);
-		}
-		int n;
-
-		for (n = 65; n < 90; n++)
-		{
-				putchar (n);
-		}
+			}
+	}
 		return (0);
 }
