fb40ccf60dea0e9f769ee0da3f40e2b51c1a263b
Jose Velez <jose-0919@hotmail.com>
2020-06-12 16:13:28 -0500

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+	 * main - alfabeto mayusculas y minisculas
+	 *
+	 * Return: 0
+	 */
+
+	int main(void)
+	{
+		int i = 0;
+				for (i = 48; i < 58; i++)
+			{
+			putchar (i);
+			}
+		for (i = 97; i < 103; i++)
+			{
+				putchar (i);
+			}
+	 putchar ('\n')
+		return (0);
