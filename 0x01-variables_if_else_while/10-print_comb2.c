3ca99c0fda036125a5d4299d687af011ca8efa15
Jose Velez <jose-0919@hotmail.com>
2020-06-12 17:10:04 -0500

    Task 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Prints the numbers from 00 to 99
+ * Return: 0
+ */
+int main(void)
+{
+	int  i = 48;
+	int  j = 48;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i == 57 && j == 57)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
