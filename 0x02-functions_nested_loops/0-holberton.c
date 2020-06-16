46848f36bccf6e597804295f0da954393442eef5
Jose Velez <jose-0919@hotmail.com>
2020-06-16 17:17:45 -0500

    Task 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,9 +8,10 @@
 int main(void)
 
 {
-	char palabra [9] = "Holberton";
+	char palabra[9] = "Holberton";
 	int i;
-	for(i = 0; i < 9; i++ ){
+	for (i = 0; i < 9; i++)
+	{
 		_putchar(palabra[i]);
 	}
 	_putchar('\n');
