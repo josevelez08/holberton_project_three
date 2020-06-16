97c9ae47f38ce08cc4a03ac859e35e3e7deb52e6
Jose Velez <jose-0919@hotmail.com>
2020-06-16 17:12:37 -0500

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
- * main - print message code c
+ * print_alphabet - 
  *
- * Return: 0 always runs
+ * Return: void
  */
 
 void print_alphabet(void)
@@ -14,5 +14,4 @@ void print_alphabet(void)
 		 _putchar(i);
 	}
 	putchar('\n');
-	return (0);
 }
