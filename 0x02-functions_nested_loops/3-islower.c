f6d69deff59140e60f116b4c6b50237a180b0a18
Jose Velez <jose-0919@hotmail.com>
2020-06-16 18:36:11 -0500

    Tasks

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_alphabet - prints the alphabet
+ *
+ * Return: Void
+ */
+
+int _islower(int c)
+
+{
+	if ( _islower (c))
+   return (1);
+	 else 
+	 return (0);
+}
