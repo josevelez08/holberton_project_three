3fc9ad0a2b4d84ebb33a7edb628b61dec70b85e6
Jose Velez <jose-0919@hotmail.com>
2020-06-16 11:09:32 -0500

    Tasks

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
-int print(int i);
+#include "holberton.h"
+/**
+ * main - print message code c
+ *
+ * Return: 0 always runs
+ */
 
-main()
+int main(void)
 {
-	printf("%s\n", "Holberton");
+	char palabra[9] = "Holberton";
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		 _putchar(palabra[i]);
+	}
+	putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - print message code c
+ *
+ * Return: 0 always runs
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		 putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
