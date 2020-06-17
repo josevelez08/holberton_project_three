0999c19b6b8bc234a9df31a6912b0acdc756b154
Jose Velez <jose-0919@hotmail.com>
2020-06-16 22:26:18 -0500

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_sign - mayor o menor
+ * @n: character to be checker
+ * Return: 1 is true or 0 is false
+ */
+
+int print_sign(int n);
+
+{
+	
+	if (n > 0)
+	{
+		return (1);
+		print_sign (+)
+	}
+	else if (n == 0)
+	{ 
+		return
+	}
+}
