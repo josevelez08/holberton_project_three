13b50ce013ca13aed237682159a1221a341deae7
Jose Velez <jose-0919@hotmail.com>
2020-06-16 20:56:20 -0500

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,27 +2,20 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * print_alphabet - prints the alphabetx
- *
- * Return: 1 or 0
+ * _isalpha - check characters
+ * @c character to be checker
+ * Return: 1 is true or 0 is false
  */
 
 int _isalpha(int c)
 
 {
-	if ( c >= 'a' && c <= 'z')
+	if (( c <= 'a' && c >= 'z') ||  ( c <= 'A' && c >= 'Z'))
 	{
 		return (1);
 	}
 	else 
 	{
-		if ( c >= 'A' && c <= 'Z')
-		{
-			return (1);
-		}
-		else
-		{
 			return (0);
-		}
 	}
 }
