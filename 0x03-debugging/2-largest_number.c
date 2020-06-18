714c2c06d7813005d975cd452780d93bd0c7778d
Jose Velez <jose-0919@hotmail.com>
2020-06-18 17:34:18 -0500

    Task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,17 +10,13 @@
 	{
 		int largest;
 
-		if (a > b && b > c)
+		if (a > b && a > c)
 		{
 			largest = a;
 		}
-		else if (b > a && a > c)
+		else if (b > a && b > c)
 		{
 			largest = b;
-		}
-		else if (a == b)
-		{
-			largest = a;
 		}
 		else
 		{
