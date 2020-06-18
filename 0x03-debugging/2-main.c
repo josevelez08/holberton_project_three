f560481413beee09d34fcd0ff896f90140d1b6f9
Jose Velez <jose-0919@hotmail.com>
2020-06-18 17:55:35 -0500

    main 2

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -19,5 +19,21 @@ int main(void)
 
 	printf("%d is the largest number\n", largest);
 
+	a = -972;
+	b = -98;
+	c = 0;
+
+	largest = largest_number(a, b, c);
+
+	printf("%d is the largest number\n", largest);
+
+	a = 0;
+	b = 100;
+	c = -10;
+
+	largest = largest_number(a, b, c);
+
+	printf("%d is the largest number\n", largest);
+
 	return (0);
 }
