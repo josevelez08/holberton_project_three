1e16fda7a8970e68d694233f0b42d3b897306044
Jose Velez <jose-0919@hotmail.com>
2020-06-17 21:39:47 -0500

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,20 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
+	* main - check the code for Holberton School students.
+	*
+	* Return: Always 0.
+	*/
 
 int _islower(int c)
 
 {
-  if ( c >= 'a' && c <= 'z')
-  {
-    return (1);
-  }
-   else
+	if (c >= 'a' && c <= 'z')
+		{
+		return (1);
+		}
+	else
 	{
-	  return (0);
- 	}
+		return (0);
+	}
 }
