9d0256d4df4fef17cd800fe29ec07bd544dda492
Jose Velez <jose-0919@hotmail.com>
2020-06-17 21:42:03 -0500

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
-	* main - check the code for Holberton School students.
-	*
+	* _islower - check if the character is lowercase .
+	*@c character to be checked
 	* Return: Always 0.
 	*/
 
