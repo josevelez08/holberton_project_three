7148e73aaf4d57f01ffb5ef42b2a2bff3813e76e
Jose Velez <jose-0919@hotmail.com>
2020-06-18 02:19:43 -0500

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,17 +9,23 @@ void print_to_98(int n)
 {
 	while (n != 98)
 	{
-		printf("%d",n);
+		printf("%d", n);
 		_putchar(44);
 		_putchar(32);
 	}	
 	if (n < 98)
 	{
 		n++;
+		printf("%d", n);
+		_putchar(44);
+		_putchar(32);
 	}
 	else
 	{
 		n--;
+		printf("%d", n);
+		_putchar(44);
+		_putchar(32);
 	}
 	printf("%d",98);
 }
