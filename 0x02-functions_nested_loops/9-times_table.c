ff4dd15007f8cc6bd158871dfed26cf97f691bbd
Jose Velez <jose-0919@hotmail.com>
2020-06-18 12:50:43 -0500

    betty

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,39 +2,40 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * times_table - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 void times_table(void)
 {
 	int i;
-	
+
 	int j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++ )
+		for (j = 0; j < 10; j++)
 		{
 			int resultado;
+
 			resultado = i * j;
-			if ( j != 9)
-			{	
-				if ( resultado < 10 )
+			if (j != 9)
+			{
+				if (resultado < 10)
 				{
-					printf(" %d, "  , resultado);
+					printf(" %d, ", resultado);
 				} else
 				{
-					printf("%d, "  , resultado);
+					printf("%d, ", resultado);
 				}
 			} else
 			{
 				if ( resultado < 10 )
 				{
-					printf(" %d$\n"  , resultado);
+					printf(" %d$\n", resultado);
 				} else
 				{
-					printf("%d$\n"  , resultado);
+					printf("%d$\n", resultado);
 				}
 			}
 		}
