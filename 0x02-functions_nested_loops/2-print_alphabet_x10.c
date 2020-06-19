bd1c9aa2f40e37256331d1796a086f9b638cf501
Jose Velez <jose-0919@hotmail.com>
2020-06-18 21:31:42 -0500

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include "holberton.h"
 
-/**
-  * main - check the code for Holberton School students.
-  *
-  * Return: Always 0
-  */ 
+  /**
+* main - check the code for Holberton School students.
+*
+* Return: Always 0
+*/
 void print_alphabet_x10(void)
 {
-  char i;
-  int j;
-  for (j = 1; j <= 10; j++)
-  {
-    for (i = 'a'; i <= 'z'; i++)
-    {
-      _putchar(i);
-      _putchar('\n');
-    }
-    _putchar('\n');
-  }
+	char i;
+	int j;
+
+	for (j = 1; j <= 10; j++)
+	{
+		for (i = 97 ; i < 123 ; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+		_putchar('\n');
+	}
 }
