16ae13d945655a7a1e655135145ce42b9c68c754
Jose Velez <jose-0919@hotmail.com>
2020-06-18 21:44:34 -0500

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,12 +11,11 @@ void print_alphabet_x10(void)
 	char i;
 	int j;
 
-	for (j = 1; j <= 10; j++)
+	for (j = 0; j <= 10; j++)
 	{
 		for (i = 97 ; i < 123 ; i++)
 	{
 		_putchar(i);
-		_putchar('\n');
 	}
 		_putchar('\n');
 	}
