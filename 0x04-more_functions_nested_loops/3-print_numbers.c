5cd1916ca565700e6500cf48bcc337ec99e90a36
Jose Velez <jose-0919@hotmail.com>
2020-06-20 17:28:56 -0500

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
+
 /**
  * main - check the code for Holberton School students.
  *
- * Return: Always 0.
+ *
  */
 void print_numbers(void)
 {
  char c = 0 ;
 
-	for ( c >= 48 ; c < 58; c++ )
+	for (c >= 48 ; c < 58; c++)
 	{
 		_putchar (c);
 	}
