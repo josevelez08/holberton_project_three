9eff9a32034f626804c4a4064ae44827cdbbe20e
Jose Velez <jose-0919@hotmail.com>
2020-06-20 18:43:48 -0500

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void more_numbers(void)
+{
+	int f;
+	int n;
+
+	for (f = 0; f < 10; f++)
+	{
+	 for ( n = 48; n < 63; n++);
+	 {
+		 if ( n < 58)
+		 {
+			 _putchar (n);
+		 }
+		 else
+		 {
+			 for ( n = 48; n < 53; n++)
+			 {
+				 	_putchar (n);
+			 }
+		 }
+		 _putchar ("\n");
+	 }
+	}
+}
