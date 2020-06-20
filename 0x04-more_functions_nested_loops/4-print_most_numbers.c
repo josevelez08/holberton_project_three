11f0684c0ae9954c2be8040a4962b44b5de5bcae
Jose Velez <jose-0919@hotmail.com>
2020-06-20 18:10:09 -0500

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	 int c;
+		for (c = 48 ; c < 58; c++)
+		{
+			if ( c != 50 || c != 52)
+			{
+		_putchar (c);
+			}
+		}
+		_putchar('\n');
+}
