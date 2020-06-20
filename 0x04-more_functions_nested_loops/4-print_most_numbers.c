18258b22956d3e203fd201aaddde71f6a658d628
Jose Velez <jose-0919@hotmail.com>
2020-06-20 18:17:04 -0500

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,9 +11,9 @@ void print_most_numbers(void)
 	 int c;
 		for (c = 48 ; c < 58; c++)
 		{
-			if ( c != 50 || c != 52)
+			if ( c != 50 && c != 52)
 			{
-		_putchar (c);
+				_putchar (c);
 			}
 		}
 		_putchar('\n');
