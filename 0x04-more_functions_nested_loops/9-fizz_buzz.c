027f1779d31ce50e02b7091997e8229fdcc64c54
Jose Velez <jose-0919@hotmail.com>
2020-06-20 23:20:28 -0500

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void fizz(void)
+{
+	int i;
+	
+	int mod3;
+	
+	int mod5;
+
+	for (i = 1; i <= 100; i++)
+	{
+		mod3 = i % 3;
+		mod5 = i % 5;
+		if ( mod3 == 0)
+		{
+			printf("Fizz");
+		}
+		if( mod5 == 0)
+		{
+			printf("Buzz");
+		}
+		if( mod3!= 0 && mod5!=0)
+		{
+			printf("%d", i);
+		}
+			if(i != 100)
+		{
+			printf(" ");
+		}
+	}
+}
