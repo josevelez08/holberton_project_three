f89c728ab000ee94ed012cc95a405f0fd399d957
Jose Velez <jose-0919@hotmail.com>
2020-06-20 22:16:24 -0500

    Tasks

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - check the code for Holberton School students.
+ *
+ * @n: something
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for(j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar(10);
+		}
+	}
+	_putchar(10);
+}
