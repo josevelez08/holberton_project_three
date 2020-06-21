cd5bee31a5b32e8a6491e35e137e72d71a78c8bc
Jose Velez <jose-0919@hotmail.com>
2020-06-20 19:16:20 -0500

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,19 +13,17 @@ void more_numbers(void)
 
 	for (f = 0; f < 10; f++)
 	{
-	 for ( n = 0; n <= 14; n++);
-	 {
-		 if ( n < 10)
-		 {
-			 _putchar (n + 48);
-		 }
-		 else
-		 {
-			_putchar(49);
-			int mod; 
-			 mod = n % 10;
-		 	_putchar (mod + 48);
-		 }
+		for ( n = 0; n <= 14; n++);
+		{
+			if ( n < 10)
+			{
+				_putchar (n + 48);
+			}
+			else
+			{
+				_putchar(49);
+				_putchar (n % 10);
+			}
 		 _putchar (10);
 	 }
 	}
