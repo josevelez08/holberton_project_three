93796a39b331344fe90d067e030de3a00b7f2416
Jose Velez <jose-0919@hotmail.com>
2020-06-20 19:40:42 -0500

    Tasks

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: A
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		_putchar(95);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
