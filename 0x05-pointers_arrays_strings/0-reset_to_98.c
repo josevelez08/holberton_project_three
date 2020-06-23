c9de4f3f97cd3eea7989f9ede4f2cf22d38964e1
Jose Velez <jose-0919@hotmail.com>
2020-06-23 16:17:26 -0500

    Tasks

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	int n[2]={402,98};
+	return (0);
+}
