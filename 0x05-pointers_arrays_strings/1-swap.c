a7a856e99071e74ed35e9901cef9da100ecb6f32
Jose Velez <jose-0919@hotmail.com>
2020-06-23 17:02:30 -0500

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,7 +4,8 @@
 /**
  * swap_int - check the code for Holberton School students.
  *
- * Return: Always 0.
+ * @a: number 1 int input
+ * @b: number 2 int input
  */
 void swap_int(int *a, int *b)
 {
