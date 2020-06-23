ea99b70db89b2423083c98e680e611f288914198
Jose Velez <jose-0919@hotmail.com>
2020-06-23 16:36:18 -0500

    Tasks

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
