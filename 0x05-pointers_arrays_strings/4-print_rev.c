ee52947b34879b6f4a5c3399913a4783a3f761f6
Jose Velez <jose-0919@hotmail.com>
2020-06-23 23:01:27 -0500

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int j;
+
+	i = *s;
+	for ( j = 0; j = i; j-- );
+	{
+		_putchar(i);
+	}
+}
