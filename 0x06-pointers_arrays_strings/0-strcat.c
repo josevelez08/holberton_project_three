4a490571e90d1a40161e9a8f21fc7487e1e5a429
Jose Velez <jose-0919@hotmail.com>
2020-06-25 16:04:13 -0500

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,12 +4,21 @@
 /**
  * main - check the code for Holberton School students.
  *
+ * i = tamaño
  * Return: Always 0.
  */
 
 char *_strcat(char *dest, char *src)
 {
-	char *c;
-	c = &dest, &src ;
-	printf (c, '\n');
-	return (c);
+	int i, l, j;
+
+	while ( *(dest + i) != 0);
+	{
+		i = i + 1;
+	}
+	while ( *(dest + i) != 0);
+	{
+		i = i + 1;
+	}
+ return (dest);
+}
