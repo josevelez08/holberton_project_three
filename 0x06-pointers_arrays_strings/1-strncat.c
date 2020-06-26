787373b0803f8d0fd5787e2fc0445696010dcd7d
Jose Velez <jose-0919@hotmail.com>
2020-06-26 15:15:44 -0500

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,15 +5,18 @@
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
- */
+*/
 
 char *_strncat(char *dest, char *src, int n)
 {
 	int contd;
 	int conts;
 
-	for (contd = 0; dest[contd] != '\n'; contd++);
-	for (conts = 0; src[conts] < n;) 
+	for (contd = 0; dest[contd] != '\0'; contd++);
+	{
+		dest[contd] = 'W';
+	}
+	for (conts = 0; src[conts] < n && src[conts] != '\0';) 
 	{
 		dest[contd] = src[conts];
 		contd++;
@@ -21,3 +24,4 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	return (dest);
 }
+
