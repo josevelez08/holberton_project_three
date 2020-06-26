50277470e9db262535dbb3e43d143a71daa06dc2
Jose Velez <jose-0919@hotmail.com>
2020-06-26 14:08:54 -0500

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,12 +12,12 @@ char *_strncat(char *dest, char *src, int n)
 	int contd;
 	int conts;
 
-	for (contd = 0; dest[contd] != '\0'; contd++);
-
-	for (conts = 0; src[conts] < n; conts++) 
+	for (contd = 0; dest[contd] != '\n'; contd++);
+	for (conts = 0; src[conts] < n;) 
 	{
 		dest[contd] = src[conts];
 		contd++;
+		conts++;
 	}
 	return (dest);
 }
