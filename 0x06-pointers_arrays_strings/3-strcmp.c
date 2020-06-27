9d23ad612fcb673efe00e69a9432e7564a2d6201
Jose Velez <jose-0919@hotmail.com>
2020-06-27 18:12:16 -0500

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,20 +9,20 @@
 
 int _strcmp(char *s1, char *s2)
 {
-    int conts2;
-    int c;
+	int conts2;
+	int c;
 
-    for (conts2 = 0; s2[conts2] != '\0'; conts2++)
-    {
-    if ( (s1[conts2]) - (s2[conts2]) != 0)
-    {
-        c = (s1[conts2]) - (s2[conts2]);
-        break;
-    }
-    else 
-    {
-      c  = 0; 
-    }
-    }
-  return (c);
+	for (conts2 = 0; s2[conts2] != '\0'; conts2++)
+	{
+		if ((s1[conts2]) - (s2[conts2]) != 0)
+		{
+				c = (s1[conts2]) - (s2[conts2]);
+				break;
+		}
+		else 
+		{
+			c  = 0; 
+		}
+	}
+return (c);
 }
