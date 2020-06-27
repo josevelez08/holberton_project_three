7e5941fbdc8a45e1da32b91df2690ac23512c122
Jose Velez <jose-0919@hotmail.com>
2020-06-27 18:07:18 -0500

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+    int conts2;
+    int c;
+
+    for (conts2 = 0; s2[conts2] != '\0'; conts2++)
+    {
+    if ( (s1[conts2]) - (s2[conts2]) != 0)
+    {
+        c = (s1[conts2]) - (s2[conts2]);
+        break;
+    }
+    else 
+    {
+      c  = 0; 
+    }
+    return (c);
+    }
+}
