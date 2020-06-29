fa0181977f02191eea5ceaab098cff232fe1fa28
Jose Velez <jose-0919@hotmail.com>
2020-06-29 14:45:25 -0500

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{ 
+  int i;
+  
+  for ( i = 0; i < n; i++)
+  {
+    *(s + i) = b;
+  }
+  return (s);
+}
