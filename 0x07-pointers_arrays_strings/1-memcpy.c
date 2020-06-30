5dc981bb32a03a6bf1d6eaa3066f87ab6500e21f
Jose Velez <jose-0919@hotmail.com>
2020-06-29 20:31:00 -0500

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; src[i] < n; i++)
+	{
+		 src[i] = dest;
+	}
+}
