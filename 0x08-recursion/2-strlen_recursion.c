48e29932caa6330544f7f68f2c95927aba9cf18f
Jose Velez <jose-0919@hotmail.com>
2020-07-01 15:53:24 -0500

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - returns the leght of a string
+ * @s: the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int tamano = 0;  
+
+	if (*s != '\0')
+	{ 
+		tamano = 1 + _strlen_recursion ( s + 1);
+	}
+	return (tamano);
+}
