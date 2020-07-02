31ccc30d3de91163c0ed6f272b3f09a01e6f8eb2
Jose Velez <jose-0919@hotmail.com>
2020-07-01 23:23:43 -0500

    Tasks

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,6 +1,7 @@
 #ifndef HOLBERTON
 #define HOLBERTON
 int _putchar(char c);
+void _print_rev_recursion(char *s);
 void _puts_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
