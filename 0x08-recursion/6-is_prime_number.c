97ea67f954a069495a2d633d04c0ac17b9262baa
Jose Velez <jose-0919@hotmail.com>
2020-07-04 02:06:26 +0000

    Tasks

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,30 +2,44 @@
 
 /**
 * is_prime_number - numeros primos
-* @n: num checker  
+* @n: num checker
+* Return: 0 always
 */
 
-int is_prime_number(int n)
-{ 
-	return find_prime_number(n, 2);
-} 
 
 int  find_prime_number(int n, int i)
 {
-	if (n <= 2) 
+	if (n <= 2)
 	{
-		if(n == 2){
-			return 1;
-		}else {
-			return 0;
+		if (n == 2)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
 		}
 	}
-	else if ( n% i == 0)
+
+	else if (n % i == 0)
 	{
-		return 0;
-	} else if ( i * i> n){
-		return 1;
+		return (0);
 	}
-		
-	return find_prime_number(n, i+1 );
-} 
+	else if (i * i > n)
+	{
+		return (1);
+	}
+
+	return ( find_prime_number(n, i + 1));
+}
+
+/**
+* is_prime_number - check
+* @n: checker
+* Return: 0 always
+*/
+
+int is_prime_number(int n)
+{
+	return (find_prime_number(n, 2));
+}
