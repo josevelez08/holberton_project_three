4164e1285a7d91f0700f23c708032046f389eabf
Jose Velez <jose-0919@hotmail.com>
2020-07-05 21:49:51 +0000

    Tasks

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,13 @@
-
 #include  <stdio.h>
+/**
+* main - contar los argumentos
+* @argc: numero de argumentos
+* @argv: no gunciona
+* Return: 0 Always
+*/
 
-
-int main (int  argc __attribute__((unused)), char* argv[])
+int main(int  argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
