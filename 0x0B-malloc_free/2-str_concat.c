22f0d49421d256904f0078e5bad5fe3621183475
Jose Velez <jose-0919@hotmail.com>
2020-07-10 02:59:44 +0000

    Tasks

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,28 +2,29 @@
 #include <stdlib.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * str_concat - check the code for Holberton School students.
+ * @s1: primero
+ * @s2: segundo
  * Return: Always 0.
  */
 char *str_concat(char *s1, char *s2)
 {
 	unsigned int cont1, cont2;
 	char *p;
-	
-	if ( s1 == NULL)
+
+	if (s1 == NULL)
 	{
 		return (NULL);
 	}
-		if ( s2 == NULL)
+		if (s2 == NULL)
 	{
 		return (NULL);
 	}
-	for ( cont1 = 0;  s1[cont1] != '\0';)
+	for (cont1 = 0;  s1[cont1] != '\0';)
 	{
 	cont1++;
 	}
-	for ( cont2 = 0; s2[cont2] != '\0';)
+	for (cont2 = 0; s2[cont2] != '\0';)
 	{
 	cont2++;
 	}
@@ -33,12 +34,12 @@ char *str_concat(char *s1, char *s2)
 	if (p == NULL)
 	{
 		return (NULL);
-	} 
-	for ( cont1 = 0;  s1[cont1] != '\0'; cont1++)
+	}
+	for (cont1 = 0;  s1[cont1] != '\0'; cont1++)
 	{
 	p[cont1] = s1[cont1];
 	}
-	for ( cont2 = 0;  s2[cont2] != '\0';cont2++)
+	for (cont2 = 0;  s2[cont2] != '\0'; cont2++)
 	{
 	p[cont1 + cont2] = s2[cont2];
 	}
