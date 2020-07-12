b856457de226c56b17ec1fba3082093c1ceadcee
Jose Velez <jose-0919@hotmail.com>
2020-07-12 02:57:25 +0000

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,24 +13,38 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i, j;
-char *p;
+	unsigned int i, j;
+	char *p;
 
-if (s1 == NULL && s2 == NULL)
-{
-	return (NULL);
-}
+	if (s1 == NULL && s2 == NULL)
+	{
+		return (NULL);
+	}
 
-p = (malloc((sizeof(char *) * n)));
+	p = (malloc((sizeof(char *) * n)));
 
-for (i = 0; s1[i] != '\0';)
-{
-	p[i] = s1[i];
-	i++;
-}
+	for (i = 0; s1[i] != '\0';)
+	{
+		p[i] = s1[i];
+		i++;
+	}
+
+	for (j = 0; s2[j] != '\0'; j++)
+	;
+
+	if (n < j)
+	{
 	for (j = 0; j < n; j++)
-{
-	p[i + j] = s2[j];
-}
-return (p);
+	{
+		p[i + j] = s2[j];
+	}
+	}
+	else
+	{
+	for (j = 0; j != '\0'; j++)
+	{
+		p[i + j] = s2[j];
+	}
+	}
+	return (p);
 }
