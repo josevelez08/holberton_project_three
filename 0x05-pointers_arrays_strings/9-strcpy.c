e554b73986ec31d5d7351d4486fff8bf499b13b1
Jose Velez <jose-0919@hotmail.com>
2020-07-12 08:53:32 +0000

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* _strcpy - check the code for Holberton School students.
+* @dest: destino
+* @src: fuente
+* Return: Always 0.
+*/
+char *_strcpy(char *dest, char *src)
+{
+ char *p;
+	p = dest;
+while ((*dest++ = *src++))
+{; }
+
+return (p);
+}
