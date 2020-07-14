dbc3813f92334ffdf829678f3d924a6be0566b55
Jose Velez <jose-0919@hotmail.com>
2020-07-14 01:36:04 +0000

    Tasks

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,10 +1,20 @@
 #include "dog.h"
-/*
-* init_dog:  initianilize a variable
-*
-*/
+#include <stdio.h>
+/**
+ * init_dog - initialize a variable
+ * @d: pointer d
+ * @name: Name dog
+ * @age: age dog
+ * @owner: owner dog
+ *
+ * Return: void
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+		if (d == NULL)
+	{
+		return;
+	}
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - check the code for Holberton School students.
+ * @d: pointer
+ * Return: Always 0.
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+if (d->name == NULL)
+{
+	printf("Name: (nill)\n");
+}
+else
+{
+	printf("Name %s\n", d->name);
+}
+
+printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+{
+	printf("Owner: (nill)");
+}
+else
+{
+	printf("Owner: %s\n", d->owner);
+}
+}
