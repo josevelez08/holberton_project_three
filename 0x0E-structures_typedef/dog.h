19ec8dbf85ed00bccb55c91d2c058466948dc980
Jose Velez <jose-0919@hotmail.com>
2020-07-14 01:54:44 +0000

    Task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,19 @@
 #ifndef HOLBERTON
 #define HOLBERTON
-/*
-* struct dog: perros
-*/
-struct dog
+/**
+ * struct dog - Short description
+ * @name: Name dog
+ * @age: age dog
+ * @owner: owner dog
+ *
+ * Description: date dog
+ */
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
